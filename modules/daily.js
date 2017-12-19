02127dfb166a1aef9913370ed52fa999ae804343
Tejaram <tejaram.nidamanuru@sjsu.edu>
2017-12-19 15:12:49 -0800

    DailyStatsUserhardcoded

--- modules/daily.js
+++ modules/daily.js
@@ -22,47 +22,14 @@ exports.execute = (req, res) => {
             console.log(userInfo);
             userId = userInfo.user_id;
             console.log('Tejaram'+userId);
-            let q = "SELECT id, Activity_Calories__c, Calories_Burned__c, Date__c, Steps__c, Distance__c,Sedentary_Minutes__c,"+
-                    " Floors__c, Duration__c, Note__c, Image__c, User__r.fullphotoURL, User__r.Name "+
-                    "FROM Daily_FitConnect__c where User__c = '"+userId+"'' AND date__c = TODAY limit 1";
-            force.query(oauthObj, q)
-                .then(data => {
-                    let accounts = JSON.parse(data).records;
-                    console.log(accounts);
-                    if (accounts && accounts.length>0) {
-                        let attachments = [];
-                        accounts.forEach(function(account) {
-                            let fields = [];
-                            fields.push({title: "Steps", value: account.Steps__c, short:true});
-                            fields.push({title: "Distance", value: account.Distance__c, short:true});                    
-                            fields.push({title: "Calories", value: account.Calories_Burned__c, short:true});
-                            fields.push({title: "Floors", value: account.Floors__c, short:true});
-                            fields.push({title: "Sedentary Minutes", value: account.Sedentary_Minutes__c, short:true});
-                            fields.push({title: "Activity Minutes", value: (account.Duration__c/60), short:true});
-                            fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + account.Id, short:false});
-                            attachments.push({color: "#7F8DE1", fields: fields});
-                        });
-                        res.json({text: "FitChallenges: ", attachments: attachments});
-                    } else {
-                        res.send("There are no FitChallenges going on right now!");
-                    }
-                })
-                .catch(error => {
-                    if (error.code == 401) {
-                        res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
-                    } else {
-                        console.log(error);
-                        res.send("An error as occurred");
-                    }
-                });
         })
         .catch(error => {
             console.log(error);            
         });
         console.log('userId'+userId);
-    /*let q = "SELECT id, Activity_Calories__c, Calories_Burned__c, Date__c, Steps__c, Distance__c,Sedentary_Minutes__c,"+
+    let q = "SELECT id, Activity_Calories__c, Calories_Burned__c, Date__c, Steps__c, Distance__c,Sedentary_Minutes__c,"+
         " Floors__c, Duration__c, Note__c, Image__c, User__r.fullphotoURL, User__r.Name "+
-            "FROM Daily_FitConnect__c where User__c = '"+userId+"'' AND date__c = TODAY limit 1";
+            "FROM Daily_FitConnect__c where User__c = '00546000000tsGYAAY' AND date__c = TODAY limit 1";
         //q = "select Id, Name, Status__c,End__c,start__c,Winning_Score2__c,Type_Unit__c,Winner__c,Type__c,Prize__c from "+
         //    "FitChallenge__c WHERE Name LIKE '%" + req.body.text + "%'  order by start__c desc LIMIT 2";
         //q = "SELECT Id, Name, Phone, BillingAddress FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
@@ -97,5 +64,5 @@ exports.execute = (req, res) => {
                 console.log(error);
                 res.send("An error as occurred");
             }
-        });*/
+        });
 };
